f9588ce10786d83b065516d7a05f1e83663d8e3a
Riddled Ocean <ashton.wamsley@bsu.edu>
2026-03-21 20:46:39 -0400

    Created functions for updating kb and choosing moves and initialized steps function.

--- wumpus_tiktaktoe.py
+++ wumpus_tiktaktoe.py
@@ -32,4 +32,68 @@ class WumpusWorld:
         return cell == self.wumpus
     
 class WumpusKBAgent:
-    
+    def __init__(self, world):
+        self.world = world
+        self.sp = world.start
+        self.alive = True
+        self.goal_reached = False
+        self.visited = set()
+        self.safe = set([self.ps])
+        self.no_pit = set([self.ps])
+        self.no_wumpus = set([self.ps])
+        self.possible_pit = set()
+        self.possible_wumpus = set()
+        self.trace = []
+        self.states_expanded = 0
+
+    def update_kb(self, cell, percept):
+        self.visited.add(cell)
+        breeze = percept["breeze"]
+        stench = percept["stench"]
+        adjacent = list(self.world.neighbors(cell))
+
+        if not breeze:
+            for n in adjacent:
+                self.no_wumpus.add(n)
+                self.safe.add(n)
+                if n in self.possible_wumpus:
+                    self.possible_wumpus.remove(n)
+        else:
+            for n in adjacent:
+                if n not in self.no_wumpus and n not in self.safe:
+                    self.possible_wumpus.add(n)
+
+        for n in adjacent:
+            if n in self.no_pit and n in self.no_wumpus:
+                self.safe.add(n)
+
+    def choose_move(self):
+        self.states_expanded += 1
+
+        possible_moves = list(self.world.neighbors(self.ps))
+        safe_unvisited = [c for c in possible_moves if c in self.safe and c not in self.visited]
+        if safe_unvisited:
+            return safe_unvisited[0]
+        
+        safe_visited = [c for c in possible_moves if c in self.safe]
+        if safe_visited:
+            return safe_visited[0]
+        
+        unknown = [
+            c for c in possible_moves
+            if c not in self.safe
+            and c not in self.possible_pit
+            and c not in self.possible_wumpus
+        ]
+        if unknown:
+            return unknown[0]
+        
+        risky = [
+            c for c in possible_moves
+            if c not in self.possible_pit and c not in self.possible_wumpus
+        ]
+        if risky:
+            return risky[0] if risky else None
+
+    def step(self):
+        
